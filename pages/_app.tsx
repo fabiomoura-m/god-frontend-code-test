c45741dd176097765014114b697a01eda88b0f33
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-11-01 17:09:01 -0300

    feat: add and config components lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
-import { HelloWorld } from "../src/components/HelloWorld";
-import "../public/css/styles.css";
-import React from "react";
+import { HelloWorld } from '../src/components/HelloWorld';
+import '../public/css/styles.css';
+import { StyleProvider, ThemePicker } from 'vcc-ui';
+import React from 'react';
 
 function HomePage() {
-  return (
-    <React.StrictMode>
-      <HelloWorld />
-    </React.StrictMode>
-  );
+    return (
+        <React.StrictMode>
+            <StyleProvider>
+                <ThemePicker variant="light">
+                    <HelloWorld />
+                </ThemePicker>
+            </StyleProvider>
+        </React.StrictMode>
+    );
 }
 
 export default HomePage;
